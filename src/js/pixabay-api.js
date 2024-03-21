0f934c4c152961d0d3cfe03da414f07085d3bd36
NatalySheludko <137063086+NatalySheludko@users.noreply.github.com>
2024-03-21 02:25:00 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 export default function getImgGallery(imagesEl) {
-  const BASE_URL = "https://pixabay.com/api";
+  const BASE_URL = "https://pixabay.com/api/";
   const params = new URLSearchParams({
     key: "42911891-070dc2b99870cce1062cd0bc6",
     q: imagesEl,
@@ -9,5 +9,9 @@ export default function getImgGallery(imagesEl) {
   });
   const url = `${BASE_URL}?${params}`;
 
-  return fetch(url).then(res => res.json());
+  return fetch(url).then(res =>
+    res.json().catch(error => {
+      console.log(error);
+    })
+  );
 }
